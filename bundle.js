87d9eb6509554a5bbc9abd91c94377530996d0f6
Blade Barringer <blade@crookedneighbor.com>
2016-09-02 15:23:09 -0500

    fix: account for system messages in posted by text

--- bundle.js
+++ bundle.js
@@ -24029,8 +24029,16 @@ var $text = $('#flagged-message-text')
 var $meta = $('#flagged-message-meta')
 
 function displayFlaggedMessage (msg) {
+  var postedBy;
+
+  if (msg.user) {
+    postedBy = 'Posted by ' + msg.user;
+  } else {
+    postedBy = 'System Message';
+  }
+
   $text.html(md.render(msg.text))
-  $meta.html('Posted by ' + msg.user)
+  $meta.html(postedBy)
   $msg.transition('fade up')
 }
 
